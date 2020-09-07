7968a306bd6b31b1dbad9a542e40502855832a2d
qujiale <qjl000312@163.com>
2020-09-07 18:41:04 +0800

    delete a project

--- TCPFileTsp/TCP/TcpFileTransport/TcpTransFileClient/Socket.h
+++
@@ -1,7 +0,0 @@
-#ifndef SOCKET_H
-#define SOCKET_H
-
-int connect_tcp(char*, int);
-int connect_udp(char*, int);
-
-#endif // SOCKET_H
--- TCPFileTsp/TCP/TcpFileTransport/TcpTransFileServer/PassiveServer.h
+++
@@ -1,7 +0,0 @@
-#ifndef PASSIVESERVER_H
-#define PASSIVESERVER_H
-
-int create_tcp_server(int);
-int create_udp_server(int);
-
-#endif // PASSIVESERVER_H
